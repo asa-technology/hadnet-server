3447b4d5811040a285ffbf617c912d70b5d374b2
Adam Hebert <adam.hebert656@gmail.com>
2019-05-01 10:25:30 -0500

    Added community route.

--- server/app.js
+++ server/app.js
@@ -7,11 +7,15 @@ const port = process.env.PORT || 3000;
 //routers
 const auth = require('./auth');
 const businesses = require('./businesses');
+const community = require('./community');
 
 // authentication route
 app.use('/auth', auth);
 
 // businesses route
 app.use('/businesses', businesses)
+
+// community route
+app.use('/community', community);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
