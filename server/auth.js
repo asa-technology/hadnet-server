c683b4df0d728c1e443941d7a9033eeb5ab78298
Adam Hebert <adam.hebert656@gmail.com>
2019-05-02 11:29:47 -0500

    Added bodyparser and a login route to test access token.

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const bodyParser = require('body-parser');
 
-router.post('/login', (req, res) => {
+router.post('/login', bodyParser.JSON(), (req, res) => {
   console.log('User logged in!');
+  console.log('User access token:', req.body);
   res.send('Logged in!');
 })
 
