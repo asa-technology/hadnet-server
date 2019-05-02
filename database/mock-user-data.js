593909912cc71220fe9d8794ddf878a364eabfff
Jordan Estes <jordan.estes@gmail.com>
2019-05-01 19:54:23 -0500

    updated mock data

--- database/mock-user-data.js
+++ database/mock-user-data.js
@@ -8,47 +8,85 @@
 
   //BLEND THIS DATA WITH mock-account-data.js
 
-module.exports.users = [
-    {
-        account_id: 4,
-        display_name: "NolaFoodLover25",
-        email: "lindsayFrederick@gmail.com",
-        image_url: "https://www.ctvnews.ca/polopoly_fs/1.1338067.1371997734!/httpImage/image.jpg_gen/derivatives/landscape_620/image.jpg",
+  module.exports.users =[
+    {   
+        id: 1,
+        email: "autoserv@yahoo.com",
+        display_name: "Shawn Pendleton",
+        account_type: 'Business',
+        google_id: 2304,
+        url_image: "https://i.imgur.com/D6qT57Y.jpg",
+    },
+    {   
+        id: 2,
+        email: "johnandsons@gmail.com",
+        display_name: "Veronica Hemsworth",
+        account_type: 'Business',
+        google_id: 1532,
+        url_image: "https://i.imgur.com/ryZhJO6.jpg",
     },
     {
-        account_id: 5,
-        display_name: "Fred Thomas",
+        id: 3,
+        email: "beautyhair@gmail.com",
+        display_name: "Ned Stark",
+        account_type: 'Business',
+        google_id: 423,
+        url_image: "https://i.imgur.com/mFuuhgY.jpg",
+    },
+    {
+        id: 4,
+        email: "heideBrots@gmail.com",
+        display_name: "Heide Brotwurst",
+        account_type: 'User',
+        google_id: 5043,
+        url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-c4wPGgxKcCqSFAuD8xDoYv7gmZLF3vjs3rqjuMfhEljJqaenfg",
+    },
+    {
+        id: 5,
+        email: "Tbenny@supercool.com",
+        display_name: "Taylor Benedict",
+        account_type: 'User',
+        google_id: 3652,
+        url_image: "https://i.imgur.com/UEJGOSA.jpg"
+    },
+    {
+        id: 6,
+        email: "SammyK@holla.net",
+        display_name: "Sam Keer",
+        account_type: 'User',
+        google_id: 7,
+        url_image: "https://avatars3.githubusercontent.com/u/43652523?s=400&v=4"
+    },
+    {
+        id: 7,
+        email: "bailey143@gmail.com",
+        display_name: "Eustice Bailey",
+        account_type: 'User',
+        google_id: 7777,
+        url_image: "https://www.ctvnews.ca/polopoly_fs/1.1338067.1371997734!/httpImage/image.jpg_gen/derivatives/landscape_620/image.jpg",
+    },
+    {
+        id: 8,
         email: "fredTomToms@gmail.com",
+        display_name: "Fred Thomas",
+        account_type: 'User',
+        google_id: 90,
         image_url: "https://static.livebooks.com/d2f9f46aa9ed49e5861ee9958c4ceb9d/i/c2eb99db1a374c6290b548d4a9d44342/1/4SoifmQp45JMgBnHghiDJ/Faces-Mendez_Shala_17E.jpg",
     },
     {
-        account_id: 6,
-        display_name: "Shawn Hemphill",
-        email: "shemps123@gmail.com",
-        image_url: "https://s3.amazonaws.com/fundly_uploads/uploads/cf725045-2bbb-4b8c-9294-7cbe16b5ed58.JPG",
-    },
-    {
-        account_id: 7,
-        display_name: "Wendell Adams",
-        email: "wendelladams@gmail.com",
-        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_K1ZlAgw5WbB1x9Nt6hfLAnyEnjHBG_74iAtCj3eYUdDIyW4kwg",
-    },
-    {
-        account_id: 8,
-        display_name: "Wendy Branson",
-        email: "wendywends@gmail.com",
-        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-c4wPGgxKcCqSFAuD8xDoYv7gmZLF3vjs3rqjuMfhEljJqaenfg",
-    },
-    {
-        account_id: 9,
-        display_name: "Miles Porter",
+        id: 9,
         email: "milesP120@gmail.com",
+        display_name: "Miles Porter",
+        account_type: 'User',
+        google_id: 1203,
         image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFI036YpWgtC8Rw7FSGZKXLoGQCcuXY1lPnp1Td3HKLl4TCJT-",
     },
     {
-        account_id: 10,
-        display_name: "William Thomas",
+        id: 10,
         email: "willieaux@gmail.com",
+        display_name: "William Thomas",
+        account_type: 'User',
+        google_id: 3042,
         image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlnxPsORMvNI7AHnOPjEMBd8dhTwwoQhr1qV3zhs36L1UaiqMY",
     },
-]
+];
