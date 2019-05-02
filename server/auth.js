5a29adaabfa1fe588285a5787864c7b2c083fd47
Adam Hebert <adam.hebert656@gmail.com>
2019-05-02 13:31:26 -0500

    Added lodash, grabbing fb profile picture.

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 const request = require('request');
+const _ = require('lodash');
 
 const validateWithFacebook = (accessToken) => {
   return new Promise((resolve, reject) => {
@@ -18,13 +19,33 @@ const validateWithFacebook = (accessToken) => {
   })
 }
 
+const retrieveProfilePic = (accessToken, userId) => {
+  return new Promise((resolve, reject) => {
+    request({
+      url: `http://graph.facebook.com/${userId}/picture`,
+      qs: {access_token: accessToken}
+    }, (err, response, body) => {
+      if (!err) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(err);
+      }
+    })
+  })
+}
+
 router.post('/login', bodyParser.json(), (req, res) => {
   console.log('User logged in!');
   console.log('User access token:', req.body);
+  const userObj = {};
   validateWithFacebook(req.body.accessToken)
     .then((response) => {
       console.log("Response from Facebook Graph API:", response);
-      res.send('Logged in!');
+      _.extend(userObj, response);
+      return retrieveProfilePic(req.body.accessToken, userObj.id);
+    })
+    .then((response) => {
+      console.log(response);
     })
     .catch((error) => {
       console.error(error);
